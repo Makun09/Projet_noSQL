7779f03ca52f044310dfe9311599acc8fce085b1
Taqkt <yanisboussaha@outlook.fr>
2024-12-19 16:49:32 +0100

    ajout de la structure HTML de la page d'accueil

--- home/index.php
+++ home/index.php
@@ -1,5 +1,4 @@
 <?php
-
 $title = 'Home';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/include/head.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/include/header.php';
@@ -8,3 +7,19 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/utils/database.php';
 
 
 var_dump($_SESSION);
+
+?>
+
+
+<body>
+    <div class="container mt-5">
+        <div class="jumbotron">
+            <h1 class="display-4">Home</h1> 
+        </div>
+    </div>
+
+    
+
+
+
+</body>
